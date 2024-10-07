c201ce7dfbbf849b01494808f07a635f75d73920
TrustCode01 <trustcharingani@gmail.com>
2024-10-07 12:08:13 -0400

    landing page log in and sign up components added and tested all working now

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React,{useEffect} from 'react';
+import React, {/*{useEffect}*/}from 'react';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import Navbar from './Components/Navbar/Navbar';
-import Landing_Page from './Components/Landing_Page/Landing_Page'
-
+import Login from './Components/Login/Login'
 function App() {
   return (
     <div className='App'>
          {/* Set up BrowserRouter for routing */}
         <BrowserRouter>
-           <Navbar />
-           <landing_Page/>
+           <Login />
            <Routes>
                {/* Set up the Routes for different pages */}
-               <Route path="/" element={<Landing_Page/>}/>
+               
            </Routes>
         </BrowserRouter>
       
