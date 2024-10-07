106af2ee0555ecec3678712a56160d9df12ecb1c
TrustCode01 <trustcharingani@gmail.com>
2024-10-07 03:47:51 -0400

    connecting Mongodb for storing user log in info

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:1Zz4usCI2PtBoC7iVQiHa7bx@172.21.194.158:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
